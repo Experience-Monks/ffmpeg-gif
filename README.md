c86fa245ec467d0c9a489b433a32125d11a9584a
mattdesl <dave.des@gmail.com>
2016-02-19 11:41:31 -0500

    bam!

---
+++ README.md
@@ -0,0 +1,45 @@
+# ffmpeg-gif
+
+[![experimental](http://badges.github.io/stability-badges/dist/experimental.svg)](http://github.com/badges/stability-badges)
+
+A small script that converts videos to a high quality GIF, by spawning `ffmpeg` in two passes using `palettegen` filter.
+
+![anim.gif](./anim.gif)
+
+> <sup>The above GIF was generated with this tool</sup>
+
+## Install
+
+```sh
+npm install ffmpeg-gif -g
+```
+
+## Examples
+
+```sh
+# create 320w 30fps gif
+ffmpeg-gif input.mov output.gif --fps=30 --scale=320:-1
+
+# seek to 23s and make the gif 10s in length
+ffmpeg-gif input.mov output.gif --seek 23 --time 10
+
+# seek/time takes any ffmpeg duration format
+ffmpeg-gif input.mov output.gif -S 00:10:00
+```
+
+## Usage
+
+```sh
+ffmpeg-gif input output [opts]
+
+Options:
+
+  --fps, -f    fps, default 20
+  --scale, -s  resize to width:height (optional)
+  --seek, -S   seek to time, default no seek
+  --time, -t   total duration, default full clip
+```
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/Jam3/ffmpeg-gif/blob/master/LICENSE.md) for details.
---
+++ cmd.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+var spawn = require('child_process').spawn;
+var rimraf = require('rimraf');
+var flat = (a, b) => a.concat(b);
+var argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    fps: 'f',
+    scale: 's',
+    seek: 'S',
+    time: 't'
+  }
+});
+
+var input = argv._[0];
+var output = argv._[1];
+
+var suffix = '__tmp__palette_' + Date.now() + '.png';
+var tmpFileName = input + suffix;
+var needsCleanup = false;
+
+var ss = argv.start ? [ '-ss', argv.start ] : '';
+var t = argv.time ? [ '-t', argv.time ] : '';
+var inputFlag = [ '-i', input ];
+var fps = 'fps=' + (argv.fps || 20) + '';
+var scale = argv.scale ? ('scale=' + argv.scale + ':flags=lanczos') : '';
+var filterStr = [ fps, scale ].filter(Boolean).join(',');
+var filter1 = [ '-vf', filterStr + ',palettegen' ];
+var filter2 = [ '-filter_complex', filterStr + '[x];[x][1:v]paletteuse' ];
+
+var pass1Flags = [ '-y', ss, t, inputFlag, filter1, tmpFileName ].filter(Boolean).reduce(flat, []);
+var pass2Flags = [ '-y', ss, t, inputFlag, '-i', tmpFileName, filter2, '-f', 'gif', output ].filter(Boolean).reduce(flat, []);
+
+var proc = spawn('ffmpeg', pass1Flags, { stdio: 'inherit' });
+proc.on('exit', code => {
+  needsCleanup = true;
+  if (code !== 0) return bail(code);
+  var proc2 = spawn('ffmpeg', pass2Flags, { stdio: 'inherit' });
+  proc2.on('exit', code => {
+    if (code !== 0) return bail(code);
+    finish();
+  });
+});
+
+function bail (exitCode) {
+  console.error(new Error('Exited with code ' + exitCode));
+  finish();
+}
+
+function finish () {
+  if (!needsCleanup) return;
+  rimraf.sync(tmpFileName);
+  needsCleanup = false;
+}
+
+process.on('exit', () => {
+  finish();
+});
+
+process.on('SIGINT', (code) => {
+  finish();
+  process.exit(code);
+});
